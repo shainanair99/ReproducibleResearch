3642068c396c41d391c375ede9821eecd0965748
Shaina Nair <60781476+shainanair99@users.noreply.github.com>
2020-03-13 20:04:57 +0530

    Create avgDailyPattern.R

---
+++ avgDailyPattern.R
@@ -0,0 +1,9 @@
+int_avg <- tapply(df_ign$steps, df_ign$interval, mean, na.rm=TRUE, simplify=T)
+df_ia <- data.frame(interval=as.integer(names(int_avg)), avg=int_avg)
+
+with(df_ia,
+     plot(interval,
+          avg,
+          type="l",
+          xlab="5-minute intervals",
+          ylab="average steps in the interval across all days"))
