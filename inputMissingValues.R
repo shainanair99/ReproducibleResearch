8a9f365351dafae9b3ba5642b255fd294dbf1f6f
Shaina Nair <60781476+shainanair99@users.noreply.github.com>
2020-03-13 20:18:29 +0530

    Create inputMissingValues.R

---
+++ inputMissingValues.R
@@ -0,0 +1,18 @@
+##First, we calculate and report the total number of missing values in the dataset (i.e. the total number of rows with NAs)
+sum(is.na(df$steps))
+
+## [1] 2304
+
+df_impute <- df
+ndx <- is.na(df_impute$steps)
+int_avg <- tapply(df_ign$steps, df_ign$interval, mean, na.rm=TRUE, simplify=T)
+df_impute$steps[ndx] <- int_avg[as.character(df_impute$interval[ndx])]
+
+new_dailysum <- tapply(df_impute$steps, df_impute$date, sum, na.rm=TRUE, simplify=T)
+
+hist(x=new_dailysum,
+     col="red",
+     breaks=20,
+     xlab="daily steps",
+     ylab="frequency",
+     main="The distribution of daily total (with missing data imputed)")
