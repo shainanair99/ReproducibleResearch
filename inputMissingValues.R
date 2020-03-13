723743989d384ede2c49b32c99da5cfbaf418892
Shaina Nair <60781476+shainanair99@users.noreply.github.com>
2020-03-13 20:23:12 +0530

    Update inputMissingValues.R

--- inputMissingValues.R
+++ inputMissingValues.R
@@ -3,16 +3,21 @@ sum(is.na(df$steps))
 
 ## [1] 2304
 
-df_impute <- df
-ndx <- is.na(df_impute$steps)
-int_avg <- tapply(df_ign$steps, df_ign$interval, mean, na.rm=TRUE, simplify=T)
-df_impute$steps[ndx] <- int_avg[as.character(df_impute$interval[ndx])]
+```{r, echo=TRUE}
+nas <- is.na(activities$steps)
+total_empty <- sum(nas)
+percentage_empty <- mean(nas)
+# Total empty results
+print(total_empty)
+# Percentage
+print(paste(sprintf("%.2f", percentage_empty * 100), "%", sep=""))
+```
 
-new_dailysum <- tapply(df_impute$steps, df_impute$date, sum, na.rm=TRUE, simplify=T)
-
-hist(x=new_dailysum,
-     col="red",
-     breaks=20,
-     xlab="daily steps",
-     ylab="frequency",
-     main="The distribution of daily total (with missing data imputed)")
+- Using average of that 5-minute to populate NAs
+```{r, echo=TRUE}
+not_empty <- mutate(activities)
+na_intervals <- not_empty$interval[nas]
+average_na_intervals <- sapply(na_intervals, function(item) {average_of_steps_by_interval$average[average_of_steps_by_interval$interval==item]})
+not_empty$steps[nas] = average_na_intervals
+#average_of_steps_by_interval$average[average_of_steps_by_interval$interval==0]
+```
