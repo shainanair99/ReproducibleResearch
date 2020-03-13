77104805fda3f5a58bf047c59f62721d97c0fca9
Shaina Nair <60781476+shainanair99@users.noreply.github.com>
2020-03-13 19:47:23 +0530

    Update Histogram|Mean|Median.R

--- Histogram|Mean|Median.R
+++ Histogram|Mean|Median.R
@@ -1,13 +1,13 @@
-dailysum <- tapply(df_ign$steps, df_ign$date, sum, na.rm=TRUE, simplify=T)
-dailysum <- dailysum[!is.na(dailysum)]
+library(ggplot2)
+Q2<-data.frame(tapply(activity$steps,activity$date,sum,na.rm=TRUE))
+Q2$date<-rownames(Q2)
+rownames(Q2)<-NULL
+names(Q2)[[1]]<-"Total Steps taken per day"
+png("plot1.png")
+#Total Steps by date bar chart
+ggplot(Q2,aes(y=Q2$`Total Steps`,x=Q2$date))+geom_bar(stat="identity") + ylab("Frequency")+xlab("Total Steps")+ggtitle("Total Steps by date")
+dev.off()
 
-hist(x=dailysum,
-     col="red",
-     breaks=20,
-     xlab="Daily total steps",
-     ylab="Frequency",
-     main="The distribution of daily total (missing data ignored)")
- 
 ##next step is to calculate the mean and median 
 mean(dailysum)
 ## [1] 10766
